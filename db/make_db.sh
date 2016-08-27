7e678f2a7bfcc0e3f9193ff7f0600f155ddf2d15
Wayne Ma <wayne.ma@rnbtech.com.hk>
2016-08-28 00:50:28 +0800

    Refactor the code:
    Put installation in Dockerfile, and initialization in other .sh script

--- db/make_db.sh
+++ db/make_db.sh
@@ -10,3 +10,9 @@ psql -v ON_ERROR_STOP=1 --username "postgres" <<- EOSQL
     CREATE USER $MM_USERNAME WITH PASSWORD '$MM_PASSWORD';
     GRANT ALL PRIVILEGES ON DATABASE $MM_DBNAME to $MM_USERNAME;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "postgres" mattermost -c <<- EOSQL
+    CREATE EXTENSION zhparser'
+    CREATE TEXT SEARCH CONFIGURATION simple_zh_cfg (PARSER = zhparser);'
+    ALTER TEXT SEARCH CONFIGURATION simple_zh_cfg ADD MAPPING FOR n,v,a,i,e,l WITH simple;'
+EOSQL
